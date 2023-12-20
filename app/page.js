997785320763fa8af1d334c94e8b41813a37b49a
Lakshay <lakshayyadav2006@gmail.com>
2023-12-20 19:29:30 +0530

    added timer and score

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import localFont from "next/font/local";
 import encryptedCodes from "./encryptedCodes";
 import key from "/assets/504px-Pigpen.png";
@@ -14,6 +14,26 @@ export default function Home() {
 	const [index, setIndex] = useState(0);
 	const [isCorrect, setIsCorrect] = useState(null);
 	const [showAnswer, setShowAnswer] = useState(false);
+	const [score, setScore] = useState(0);
+	const [start, setStart] = useState(false);
+
+	const [highScore, setHighScore] = useState(localStorage.getItem("highScore") || 0);
+	const [highScoreTime, setHighScoreTime] = useState(localStorage.getItem("highScoreTime") || 0);
+
+	const [time, setTime] = useState(0);
+	const [running, setRunning] = useState(false);
+
+	useEffect(() => {
+		let interval;
+		if (running) {
+			interval = setInterval(() => {
+				setTime((prevTime) => prevTime + 10);
+			}, 10);
+		} else if (!running) {
+			clearInterval(interval);
+		}
+		return () => clearInterval(interval);
+	}, [running]);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -21,12 +41,32 @@ export default function Home() {
 		if (inputText === encryptedCodes[index]) {
 			setIsCorrect(true);
 			setShowAnswer(false);
+			setScore(score + 10);
 			setIndex(Math.floor(Math.random() * encryptedCodes.length));
 		} else {
 			setIsCorrect(false);
 		}
 
 		setInputText("");
+	};
+
+	const startGame = () => {
+		setScore(0);
+		setTime(0);
+		setStart(true);
+		setRunning(true);
+	};
+	const stopGame = () => {
+		setStart(false);
+		setRunning(false);
+		if (score > highScore) {
+            alert("New highscore!!")
+			setHighScore(score);
+			localStorage.setItem("highScore", score);
+
+			setHighScoreTime("0" + Math.floor((time / 60000) % 60) + ":" + ("0" + Math.floor((time / 1000) % 60)).slice(-2) + ":" + ("0" + ((time / 10) % 100)).slice(-2));
+			localStorage.setItem("highScoreTime", "0" + Math.floor((time / 60000) % 60) + ":" + ("0" + Math.floor((time / 1000) % 60)).slice(-2) + ":" + ("0" + ((time / 10) % 100)).slice(-2));
+		}
 	};
 
 	return (
@@ -37,46 +77,74 @@ export default function Home() {
 
 			<Image src={key} width={350} height={325} />
 
-			<form onSubmit={(e) => handleSubmit(e)}>
-				<div>
-					<p className="decryptText">Decrypt this text:</p>
-					<div className={`cipher ${pigpen.className}`}>
-						<p>{encryptedCodes[index]}</p>
-					</div>
+			<div>
+				<h2>Score: {score}</h2>
+				<div className="highscoreData">
+					<h3> high score: {highScore}</h3> <h3>high score time: {highScoreTime}</h3>
 				</div>
-
-				<div className="decryptInputContainer">
-					<label>Enter the decrypted text:</label>
-					<input
-						type="text"
-						value={inputText}
-						onChange={(e) => setInputText(e.target.value)}
-					/>
+				<div className="numbers">
+					<span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+					<span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+					<span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
 				</div>
+			</div>
 
-				<div className="correctAnswerDiv">
-					{isCorrect === null ? null : isCorrect ? (
-						<p className="correct">Correct! Onto the next one!</p>
-					) : (
-						<>
-							<p className="incorrect">Incorrect! Try again!</p>
-							{showAnswer && (
-								<p className="correctAnswer">
-									<strong>Correct Answer: </strong>
-									{encryptedCodes[index]}
-								</p>
-							)}
-							<button className="submitButton" onClick={() => setShowAnswer(true)}>
-								Show Correct Answer
-							</button>
-						</>
-					)}
-				</div>
-
-				<button type="submit" className="submitButton">
-					CHECK
+			<div className="startGame">
+				<button className="startBtn" onClick={() => startGame()}>
+					{" "}
+					Start{" "}
 				</button>
-			</form>
+				<button className="stopBtn" onClick={() => stopGame()}>
+					{" "}
+					Stop{" "}
+				</button>
+			</div>
+
+			{start && (
+				<form onSubmit={(e) => handleSubmit(e)}>
+					<div>
+						<p className="decryptText">Decrypt this text:</p>
+						<div className={`cipher ${pigpen.className}`}>
+							<p>{encryptedCodes[index]}</p>
+						</div>
+					</div>
+
+					<div className="decryptInputContainer">
+						<label>Enter the decrypted text:</label>
+						<input
+							type="text"
+							value={inputText}
+							onChange={(e) => setInputText(e.target.value)}
+						/>
+					</div>
+
+					<div className="correctAnswerDiv">
+						{isCorrect === null ? null : isCorrect ? (
+							<p className="correct">Correct! Onto the next one!</p>
+						) : (
+							<>
+								<p className="incorrect">Incorrect! Try again!</p>
+								{showAnswer && (
+									<p className="correctAnswer">
+										<strong>Correct Answer: </strong>
+										{encryptedCodes[index]}
+									</p>
+								)}
+								<button
+									className="submitButton"
+									onClick={() => setShowAnswer(true)}
+								>
+									Show Correct Answer
+								</button>
+							</>
+						)}
+					</div>
+
+					<button type="submit" className="submitButton">
+						CHECK
+					</button>
+				</form>
+			)}
 		</main>
 	);
 }
