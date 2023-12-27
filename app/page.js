c2b1c446585e1308d99afc5b7fe10295cea1a72a
Lakshay <lakshayyadav2006@gmail.com>
2023-12-27 21:40:31 +0530

    final changes

--- app/page.js
+++ app/page.js
@@ -14,8 +14,9 @@ export default function Home() {
 	const [index, setIndex] = useState(Math.floor(Math.random() * encryptedCodes.length));
 	const [isCorrect, setIsCorrect] = useState(null);
 	const [showAnswer, setShowAnswer] = useState(false);
-	const [score, setScore] = useState(0);
+	// const [score, setScore] = useState(0);
 	const [start, setStart] = useState(false);
+	const [won, setWon] = useState(false);
 
 	const [time, setTime] = useState(0);
 	const [running, setRunning] = useState(false);
@@ -29,9 +30,7 @@ export default function Home() {
 		} else if (!running) {
 			clearInterval(interval);
 		}
-        if (Math.floor((time / 1000) % 60) >= 5) {
-            stopGame();
-        }
+
 		return () => clearInterval(interval);
 	}, [running]);
 
@@ -41,8 +40,17 @@ export default function Home() {
 		if (inputText === encryptedCodes[index]) {
 			setIsCorrect(true);
 			setShowAnswer(false);
-			setScore(score + 10);
+			// setScore(score + 10);
 			setIndex(Math.floor(Math.random() * encryptedCodes.length));
+
+			if (Math.floor((time / 1000) % 60) <= 5) {
+				setWon(true);
+			}
+            else {
+                setWon(false);
+            }
+
+			stopGame();
 		} else {
 			setIsCorrect(false);
 		}
@@ -51,90 +59,122 @@ export default function Home() {
 	};
 
 	const startGame = () => {
-		setScore(0);
+		// setScore(0);
 		setTime(0);
 		setStart(true);
 		setRunning(true);
-        setTimeout(() => window.scrollBy(0,500), 1)
-		
+		setTimeout(() => window.scrollBy(0, 500), 1);
+		setWon(null);
 	};
 	const stopGame = () => {
+		setIsCorrect(null);
 		setStart(false);
 		setRunning(false);
 	};
 
 	return (
 		<main>
-			<div className="heading">
-				<h1>PIGPEN CIPHER</h1> <p>by ciphering club GDGPS</p>
+			<div className="container">
+				<div>
+					<div className="heading">
+						<h1>PIGPEN CIPHER</h1> <p>by ciphering club GDGPS</p>
+					</div>
+
+					<Image src={key} width={350} height={325} />
+				</div>
+
+				{start && (
+					<form onSubmit={(e) => handleSubmit(e)}>
+						<div>
+							<p className="decryptText">Decrypt this text:</p>
+							<div className={`cipher ${pigpen.className}`}>
+								<p>{encryptedCodes[index]}</p>
+							</div>
+						</div>
+
+						<div className="decryptInputContainer">
+							<label>Enter the decrypted text:</label>
+							<input
+								type="text"
+								value={inputText}
+								onChange={(e) => setInputText(e.target.value)}
+							/>
+						</div>
+
+						<div className="correctAnswerDiv">
+							{isCorrect === null ? null : isCorrect ? (
+								<p className="correct">Correct! Onto the next one!</p>
+							) : (
+								<>
+									<p className="incorrect">Incorrect! Try again!</p>
+									{showAnswer && (
+										<p className="correctAnswer">
+											<strong>Correct Answer: </strong>
+											{encryptedCodes[index]}
+										</p>
+									)}
+									<button
+										className="submitButton"
+										onClick={() => setShowAnswer(true)}
+									>
+										Show Correct Answer
+									</button>
+								</>
+							)}
+						</div>
+
+						<button type="submit" className="submitButton">
+							CHECK
+						</button>
+					</form>
+				)}
+
+				{/* {won ? !won ? (
+					<div className="won">
+						<h1>You decipherd the message in {Math.floor((time / 1000) % 60)} seconds.</h1>
+                        <h1>Advance to level 2!</h1>
+					</div>
+				) : (
+                    <div className="won">
+                        <h1>You decipherd the message in {Math.floor((time / 1000) % 60)} seconds.</h1>
+                        <h1>Advance to level 2!</h1>
+                    </div>
+                )} */}
+
+				{won && (
+					<div className="won">
+						<h1>
+							You decipherd the message in {Math.floor((time / 1000) % 60)} seconds.
+						</h1>
+						<h1>Advance to level 2!</h1>
+					</div>
+				)}
+				{won === false && (
+					<div className="won">
+						<h1>
+							You decipherd the message in {Math.floor((time / 1000) % 60)} seconds.
+						</h1>
+						<h1>Try again next time!</h1>
+					</div>
+				)}
 			</div>
 
-			<Image src={key} width={350} height={325} />
-
 			<div>
-				<h2>Score: {score}</h2>
 				<div className="numbers">
-					Time: <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+					<span>Time: </span>
 					<span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
 					<span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
 				</div>
 			</div>
 
-			<div className="startGame">
-				<button className="startBtn" onClick={() => startGame()}>
-					{" "}
-					Start{" "}
-				</button>
+			{start ? (
 				<button className="stopBtn" onClick={() => stopGame()}>
-					{" "}
-					Stop{" "}
+					STOP
 				</button>
-			</div>
-
-			{start && (
-				<form onSubmit={(e) => handleSubmit(e)}>
-					<div>
-						<p className="decryptText">Decrypt this text:</p>
-						<div className={`cipher ${pigpen.className}`}>
-							<p>{encryptedCodes[index]}</p>
-						</div>
-					</div>
-
-					<div className="decryptInputContainer">
-						<label>Enter the decrypted text:</label>
-						<input
-							type="text"
-							value={inputText}
-							onChange={(e) => setInputText(e.target.value)}
-						/>
-					</div>
-
-					<div className="correctAnswerDiv">
-						{isCorrect === null ? null : isCorrect ? (
-							<p className="correct">Correct! Onto the next one!</p>
-						) : (
-							<>
-								<p className="incorrect">Incorrect! Try again!</p>
-								{showAnswer && (
-									<p className="correctAnswer">
-										<strong>Correct Answer: </strong>
-										{encryptedCodes[index]}
-									</p>
-								)}
-								<button
-									className="submitButton"
-									onClick={() => setShowAnswer(true)}
-								>
-									Show Correct Answer
-								</button>
-							</>
-						)}
-					</div>
-
-					<button type="submit" className="submitButton">
-						CHECK
-					</button>
-				</form>
+			) : (
+				<button className="startBtn" onClick={() => startGame()}>
+					START
+				</button>
 			)}
 		</main>
 	);
